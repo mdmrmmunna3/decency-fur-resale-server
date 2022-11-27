faf27f002529332542790bee6889d0e45ec2a458
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-11-27 19:16:15 +0600

    add category and send to client site

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +9,33 @@ const app = express();
 // middelwares 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.le2w9sh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run () {
+    try {
+        const categoriesCollection = client.db('decencyFurReSale').collection('categories');
+
+
+        // get categories form database
+        app.get('/categories', async(req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            // console.log(categories);
+            res.send(categories)
+        })
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req,res) => {
     res.send('Decency Fur ReSale is running');
